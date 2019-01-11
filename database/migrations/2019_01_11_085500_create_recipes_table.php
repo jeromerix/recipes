137dba0363da0d98e87fa45f7a466b1f29ea03c8
KeesKezer <Kees_keijser@hotmail.com>
2019-01-11 12:27:48 +0100

    fixed issue with database migration

--- database/migrations/2019_01_11_085500_create_recipes_table.php
+++ database/migrations/2019_01_11_085500_create_recipes_table.php
@@ -17,14 +17,14 @@ class CreateRecipesTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->char('name',100);
-            $table->mediumText('info');
-            $table->char('kitchenware',100);
+            $table->text('info');
+            $table->char('kitchenware',50);
             $table->char('kitchen_appliance',30);
             $table->char('method',50);
             $table->integer('how_many');
             $table->char('cuisine',50);
             $table->integer('prep_time');
-            
+
 
         });
     }
