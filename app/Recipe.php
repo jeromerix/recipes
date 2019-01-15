a53f80e1e3b9fb0d55e9326d8c74c5ad9a94e462
KeesKezer <Kees_keijser@hotmail.com>
2019-01-15 15:39:50 +0100

    Added models and relations for categories,ingredients,recipes and users

--- app/Recipe.php
+++ app/Recipe.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsto('App\User');
+    }
+
+    public function ingredients()
+    {
+        return $this->hasMany('App\Ingredient');
+    }
+
+    public function kitchenwares()
+    {
+        return $this->hasMany('App\Kitchenware');
+    }
 }
