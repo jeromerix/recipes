f651708e8a1d5856247649eca966a4c3dddea050
Mansour Radmard <m.radmard@outlook.com>
2019-01-20 21:44:35 +0100

    restructured scss file and other minor styling changes

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -34,36 +34,3 @@
       </ul>
   </div>
 </nav>
-  
-  
-  
-  {{-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <img src="{{ URL::to('/images/logo-01.png') }}" alt="Sherlock Food logo">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Cntact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sign up</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> --}}
