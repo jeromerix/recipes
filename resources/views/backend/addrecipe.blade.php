be7793bebe94b4c45b66c05803abb417b1575166
Patrick <pr_it@hotmail.com>
2019-01-21 16:30:11 +0100

    changed javescript function and tabs in code

--- resources/views/backend/addrecipe.blade.php
+++ resources/views/backend/addrecipe.blade.php
@@ -1,178 +1,155 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
+    <div class="container">
 
-      @if ($errors->any())
-        {{ implode('', $errors->all('<div>:message</div>')) }}
-@endif
+        @if ($errors->any())
+            {{ implode('', $errors->all('<div>:message</div>')) }}
+        @endif
 
-    <div class="row">
-      <div class="offset-md-3 jumbotroncustom">
-          <form action="{{route('recipes.store')}}" method="POST">
-              @csrf
-
- <h2>add recipe</h2><hr>
- <div class="form-group">
- <p>Recipe name:</p>
-<input name="name" type="text" class="form-control" id="formGroupExampleInput" placeholder="Recipe name">
-</div>
-
-<div class="form-group">
-<p>Method of preparation:</p>
-<select name="method" class="form-control" id="exampleFormControlSelect1">
-    <option>Baking</option>
-    <option>Frying</option>
-    <option>Roasting</option>
-    <option>Grilling</option>
-    <option>Steaming</option>
-    <option>Poaching</option>
-    <option>Simmering</option>
-    <option>Broiling</option>
-    <option>Blanching</option>
-    <option>Braising</option>
-    <option>Stewing</option>
-    </select>
-  </div>
-
-<div class="form-group">
-<p>Sort:</p>
-<select name="sort" class="form-control" id="exampleFormControlSelect1">
-    <option>Breakfast</option>
-    <option>Dessert</option>
-    <option>Dinner</option>
-    <option>Lunch</option>
-    <option>Snack</option>
-    </select>
-  </div>
-    <div id='add'>
         <div class="row">
-            <div class='col'>
-                <select name='ingredient[]' id='ingredientsearch' class="form-control search">
-                    @foreach($ingredients as $ingredient)
-                        <option value="{{ $ingredient->id }}">{{ $ingredient->ingredient }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class='col'>
-                <input name='amount[]' type='text' class="form-control" placeholder='amount' >
-            </div>
-            <div class='col'>
-                <select name='unit[]' class="form-control">
-                    <option>g</option>
-                    <option>mg</option>
-                    <option>kg</option>
-                    <option>tbsp</option>
-                    <option>tsp</option>
-                    <option>fl oz</option>
-                    <option>ml</option>
-                    <option>dl</option>
-                    <option>l</option>
-                    <option>gill</option>
-                    <option>bag</option>
-                    <option>cloves</option>
-                    <option>pinch</option>
-                    <option>whole</option>
-                </select>
+            <div class="offset-md-3 jumbotroncustom">
+                <form action="{{route('recipes.store')}}" method="POST">
+                @csrf
+
+                    <h2>add recipe</h2><hr>
+                    <div class="form-group">
+                        <p>Recipe name:</p>
+                        <input name="name" type="text" class="form-control" id="formGroupExampleInput" placeholder="Recipe name">
+                    </div>
+
+                    <div class="form-group">
+                        <p>Method of preparation:</p>
+                            <select name="method" class="form-control" id="exampleFormControlSelect1">
+                                <option>Baking</option>
+                                <option>Frying</option>
+                                <option>Roasting</option>
+                                <option>Grilling</option>
+                                <option>Steaming</option>
+                                <option>Poaching</option>
+                                <option>Simmering</option>
+                                <option>Broiling</option>
+                                <option>Blanching</option>
+                                <option>Braising</option>
+                                <option>Stewing</option>
+                            </select>
+                    </div>
+
+                    <div class="form-group">
+                        <p>Sort:</p>
+                        <select name="sort" class="form-control" id="exampleFormControlSelect1">
+                            <option>Breakfast</option>
+                            <option>Dessert</option>
+                            <option>Dinner</option>
+                            <option>Lunch</option>
+                            <option>Snack</option>
+                        </select>
+                    </div>
+                    <div id='add'>
+                        <div class="row">
+                            <div class='col'>
+                                <select name='ingredient[]' id='ingredientsearch' class="form-control search">
+                                    @foreach($ingredients as $ingredient)
+                                        <option value="{{ $ingredient->id }}">{{ $ingredient->ingredient }}</option>
+                                    @endforeach
+                                </select>
+                                <input type='button' value='Add ingredient' onclick="add('add');">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <p>Preparation instructions:</p>
+                            <textarea name="instruction" class="form-control" id="exampleTextarea" rows="3"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <p>Number of persons:</p>
+                            <select name="how_many" class="form-control" id="exampleFormControlSelect1">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                                <option>11</option>
+                                <option>12</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <p>Cuisine:</p>
+                            <input name="cuisine" type="text" class="form-control" id="formGroupExampleInput" placeholder="Example italian">
+                        </div>
+
+                        <div class="form-group">
+                            <p>Minutes of preparation:</p>
+                            <input name="prep_time" class="form-control" type="number" value="45" id="example-number-input">
+                        <!-- </div>
+                        <p>Image:</p>
+                        <input name="image_link" type="text" class="form-control" placeholder="amount" >
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-inp" id="customFile">
+                            <label class="custom-file-label" for="customFile">Choose image</label>
+                        </div>
+                        <div class="offset-md-3"><br> -->
+                        <input type="submit" class="btn btn-success" value="{{__('Submit')}}"></button>
+                        <button type="button" class="btn btn-secondary">Go back</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-  <input type='button' value='Add extra' onclick="add('add');">
-<script>
-var counter = 1;
-function add(divName) {
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = "<div class='row'> \
+    <script>
+    var counter = 1;
+    function add(divName) {
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = "<div class='row'> \
+            <div class='col'> \
+            <select name='ingredient[]' id='ingredientsearch' class='form-control search'> \
+                @foreach($ingredients as $ingredient) \
+                <option value= '{{ $ingredient->id }}'>{{ $ingredient->ingredient }}</option> \
+                @endforeach \
+            </select> \
+        </div> \
         <div class='col'> \
-        <select name='ingredient[]' id='ingredientsearch' class='form-control search'> \
-            @foreach($ingredients as $ingredient) \
-            <option value= '{{ $ingredient->id }}'>{{ $ingredient->ingredient }}</option> \
-            @endforeach \
-        </select> \
-    </div> \
-    <div class='col'> \
-        <input name='amount[]' type='text' class='form-control' placeholder='amount' > \
-    </div> \
-    <div class='col'> \
-        <select name='unit[]' class='form-control'> \
-            <option>g</option> \
-            <option>mg</option> \
-            <option>kg</option> \
-            <option>tbsp</option> \
-            <option>tsp</option> \
-            <option>fl oz</option> \
-            <option>ml</option> \
-            <option>dl</option> \
-            <option>l</option> \
-            <option>gill</option> \
-            <option>bag</option> \
-            <option>cloves</option> \
-            <option>pinch</option> \
-            <option>whole</option> \
-        </select> \
-    </div> \
-    </div> \
-"
+            <input name='amount[]' type='text' class='form-control' placeholder='amount' > \
+        </div> \
+        <div class='col'> \
+            <select name='unit[]' class='form-control'> \
+                <option>g</option> \
+                <option>mg</option> \
+                <option>kg</option> \
+                <option>tbsp</option> \
+                <option>tsp</option> \
+                <option>fl oz</option> \
+                <option>ml</option> \
+                <option>dl</option> \
+                <option>l</option> \
+                <option>gill</option> \
+                <option>bag</option> \
+                <option>cloves</option> \
+                <option>pinch</option> \
+                <option>whole</option> \
+            </select> \
+        </div> \
+        </div> \
+    "
 
-    document.getElementById(divName).appendChild(newdiv);
-}
-</script>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+        document.getElementById(divName).appendChild(newdiv);
+    }
+    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
-<script type="text/javascript">
+    <script type="text/javascript">
 
-      $(".search").select2({
-            placeholder: "Select a ingredient",
-            allowClear: true
-        });
-</script>
-
-<div class="form-group">
-<p>Preparation instructions:</p>
-<textarea name="instruction" class="form-control" id="exampleTextarea" rows="3"></textarea>
-  </div>
-
-<div class="form-group">
-<p>Number of persons:</p>
-<select name="how_many" class="form-control" id="exampleFormControlSelect1">
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
-    <option>4</option>
-    <option>5</option>
-	  <option>6</option>
-	  <option>7</option>
-	  <option>8</option>
-	  <option>9</option>
-	  <option>10</option>
-	  <option>11</option>
-	  <option>12</option>
-    </select>
-  </div>
-
-  <div class="form-group">
-  <p>Cuisine:</p>
-  <input name="cuisine" type="text" class="form-control" id="formGroupExampleInput" placeholder="Example italian">
-  </div>
-
-  <div class="form-group">
-  <p>Minutes of preparation:</p>
-  <input name="prep_time" class="form-control" type="number" value="45" id="example-number-input">
-  </div>
-  <p>Image:</p>
-  <input name="image_link" type="text" class="form-control" placeholder="amount" >
-  <!-- <div class="custom-file">
-  <input type="file" class="custom-file-inp" id="customFile">
-  <label class="custom-file-label" for="customFile">Choose image</label>
-</div> -->
-  <div class="offset-md-3"><br>
-      <input type="submit" class="btn btn-success" value="{{__('Submit')}}"></button>
-      <button type="button" class="btn btn-secondary">Go back</button>
-      </div>
-  </form>
-      </div>
-    </div>
-  </div>
-</div>
+          $(".search").select2({
+                placeholder: "Select a ingredient",
+                allowClear: true
+            });
+    </script>
 @endsection
