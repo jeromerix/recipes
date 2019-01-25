37c310471f885ab0a3694eb185949b477b272285
Patrick <pr_it@hotmail.com>
2019-01-25 10:57:22 +0100

    added paginate and recipes from database are now shown on index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,115 +2,88 @@
 
 @section('content')
   <div class="container-fluid">
+      <a href="{{route ('recipes.create')}}" class="card-link">
+          <button type="button" class="btn btn-success">Add recipe</button>
+      </a>
     <div class="content-box-m">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card card-accordion">
-            <div class="card-header card-accordion-header text-center">
-              <h3 class="">Select your ingredients</h3>
-            </div>
-            @foreach($categories as $category)
-                <details>
-                  <summary><img src="{{ $category->icon }}">{{ $category->category }}</summary>
-                  @foreach($category->ingredients as $ingredient)
-                  <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="checkbox" name="ingredientselected[]" id="inlineCheckbox{{ $ingredient->id }}" value="{{ $ingredient->id }}">
-                      <label class="form-check-label" for="inlineCheckbox{{ $ingredient->id }}"> {{ $ingredient->ingredient }}</label>
-                  </div>
-                  @endforeach
-                </details>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-accordion">
+                    <div class="card-header card-accordion-header text-center">
+                        <h3 class="">
+                            Select your ingredients
+                        </h3>
+                    </div>
+                    @foreach($categories as $category)
+                        <details>
+                            <summary>
+                                <img src="{{ $category->icon }}">{{ $category->category }}
+                            </summary>
+                            @foreach($category->ingredients as $ingredient)
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" name="ingredientselected[]" id="inlineCheckbox{{ $ingredient->id }}" value="{{ $ingredient->id }}">
+                                    <label class="form-check-label" for="inlineCheckbox{{ $ingredient->id }}"> {{ $ingredient->ingredient }}</label>
+                                </div>
+                            @endforeach
+                        </details>
+                    @endforeach
+                </div>
+        </div>
+        @foreach($recipes as $recipe)
+            <div class="col-md-3">
+                <div class="card card-cascade card-cascade-narrower mb-5">
+                    <div class="card-view">
+                        <img class="card-img-top" src="{{ $recipe->image_link }}" alt="Recipe image">
+                    </div>
 
-            @endforeach
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card card-cascade card-cascade-narrower mb-5">
-            <div class="card-view"><img class="card-img-top" src="{{ URL::to('/images/dish-02.jpg') }}" alt="Recipe image">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Roast cod with paella & saffron olive oil</h5>
-              <div class="row row-card-info-bg">
-                <div class="col-md-4 col-no-pad"><i class="far fa-clock"></i><div       class="card-info">20 - min</div>
-                </div>
-                <div class="col-md-4 col-no-pad"><i class="fas fa-male"></i><div class="card-info">1/2</div>
-                </div>
-                <div class="col-md-4 col-no-pad"><i class="fas fa-globe-asia"></i><div class="card-info">Mexican</div>
-                </div>
-              </div>
-                <p class="card-text">Lorem ipsum dolor sit amet, quis commodi eveniet ex tempore. Asperiores voluptates fugit, quod quibusdam iure sit modi..
-                </p>
-                <a href="{{ url('/recipe') }}" class="card-link">More info</a>
-                <div>
-                  <ul class="card-social-icons">
-                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                    <li><a href="#"><i class="fas fa-envelope"></i></a></li>
-                  </ul>
-                </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card card-cascade card-cascade-narrower mb-5">
-            <div class="card-view"><img class="card-img-top" src="{{ URL::to('/images/apple-pie.jpg') }}" alt="Recipe image">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title"> Apple Pie</h5>
-              <div class="row row-card-info-bg">
-                <div class="col-md-4 col-no-pad"><i class="far fa-clock"></i><div class="card-info">20/45 - min</div>
-                </div>
-                <div class="col-md-4 col-no-pad"><i class="fas fa-male"></i><div class="card-info">8</div>
-                </div>
-                <div class="col-md-4 col-no-pad"><i class="fas fa-globe-asia"></i><div class="card-info">English</div>
-                </div>
-              </div>
-                <p class="card-text">Lorem ipsum dolor sit amet, quis commodi eveniet ex tempore. Asperiores voluptates fugit, quod quibusdam iure sit modi..
-                </p>
-              <a href="{{ url('/recipe') }}" class="card-link">More info</a>
-                <div>
-                  <ul class="card-social-icons">
-                    <li><i class="fab fa-facebook-f"></i></li>
-                      <li><i class="fab fa-twitter"></i></li>
-                      <li><i class="fab fa-google-plus-g"></i></li>
-                      <li><i class="fab fa-pinterest-p"></i></li>
-                      <li><i class="fas fa-envelope"></i></li>
-                  </ul>
+                    <div class="card-body">
+
+                        <h5 class="card-title">
+                            {{ $recipe->name }}
+                        </h5>
+                        <div class="row row-card-info-bg">
+                            <div class="col-md-4 col-no-pad">
+                                <i class="far fa-clock"></i>
+                                <div  class="card-info">
+                                    {{$recipe->prep_time}} Minutes
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-no-pad">
+                                <i class="fas fa-male"></i>
+                                <div class="card-info">
+                                    {{$recipe->how_many}}
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-no-pad">
+                                <i class="fas fa-globe-asia"></i>
+                                <div class="card-info">
+                                    {{$recipe->cuisine}}
+                                </div>
+                            </div>
+                        </div>
+                        <p class="card-text">
+                            {{str_limit ($recipe->instruction, 300)}}
+                        </p>
+                        <a href="{{route ('recipes.show',$recipe->id)}}" class="card-link">
+                            More info
+                        </a>
+                        <div>
+                            <ul class="card-social-icons">
+                                <li><i class="fab fa-facebook-f"></i></li>
+                                <li><i class="fab fa-twitter"></i></li>
+                                <li><i class="fab fa-google-plus-g"></i></li>
+                                <li><i class="fab fa-pinterest-p"></i></li>
+                                <li><i class="fas fa-envelope"></i></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card card-cascade card-cascade-narrower mb-5">
-            <div class="card-view"><img class="card-img-top" src="{{ URL::to('/images/bibimbap.jpg') }}" alt="Recipe image">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Roast cod with paella & saffron olive oil</h5>
-              <div class="row row-card-info-bg">
-                <div class="col-md-4 col-no-pad"><i class="far fa-clock"></i><div       class="card-info">60 - min</div>
-                </div>
-                <div class="col-md-4 col-no-pad"><i class="fas fa-male"></i><div class="card-info">1</div>
-                </div>
-                <div class="col-md-4 col-no-pad"><i class="fas fa-globe-asia"></i><div class="card-info">Korean</div>
-                </div>
-              </div>
-                <p class="card-text">Lorem ipsum dolor sit amet, quis commodi eveniet ex tempore. Asperiores voluptates fugit, quod quibusdam iure sit modi..
-                </p>
-                <a href="{{ url('/recipe') }}" class="card-link">More info</a>
-                <div>
-                  <ul class="card-social-icons">
-                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                    <li><a href="#"><i class="fas fa-envelope"></i></a></li>
-                  </ul>
-                </div>
-            </div>
-          </div>
-        </div>
+        @endforeach
       </div>
+    </div>
+    <div class="paginate">
+        {{ $recipes->render() }}
     </div>
   </div>
 @endsection
