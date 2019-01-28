5e5496467ed622ab9ffcd3808f2effae7971b824
Mansour Radmard <m.radmard@outlook.com>
2019-01-28 13:00:06 +0100

    made login page, styled the login form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -77,6 +77,6 @@ class RegisterController extends Controller
         $this->validator($request->all())->validate();
         event(new Registered($user = $this->create($request->all())));
         // $this->guard()->login($user);
-        return redirect($this->redirectTo)->with('message', 'Registered successfully, please login...!');
+        return redirect($this->redirectTo)->with('message', 'Registered successfully, please login!');
     }
 }
