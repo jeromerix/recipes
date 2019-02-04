3f645d86334e68ce360f3e11db420c14b8339f3f
KeesKezer <Kees_keijser@hotmail.com>
2019-02-04 14:24:39 +0100

    Working favorite functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,7 @@
 <?php
+use Illuminate\Support\Facades\Input;
 use App\Recipe;
+use Illuminate\Http\Request;
 use App\User;
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
@@ -20,8 +22,8 @@ class UserController extends Controller
     {
 
         $uid = Auth::user()->id;
-        $user = \App\User::where('id', $uid)->get();
-        $recipe = \App\Recipe::where('id', $id)->get();
+        $user = \App\User::where('id', $uid)->first();
+        $recipe = \App\Recipe::where('id', $id)->first();
         $user->favorites()->attach($recipe->id, ['favorited' => '1']);
 
     }
